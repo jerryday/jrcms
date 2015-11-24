5f1b1bd3305af8e6ad2c15a9ba9d25ec21f11f3e
jerryday <jerryday@yeah.net>
2015-11-24 07:55:13 -0500

    only listens localhost and adjust access log level to info

--- runserver.py
+++ runserver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 __author__ = 'wangdai'
 
+import logging
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
@@ -9,6 +10,8 @@ from jrcms import APP
 
 if __name__ == '__main__':
     # APP.run(debug=True)
-    http_server = HTTPServer(WSGIContainer(APP))
-    http_server.listen(5000)
+    access_log = logging.getLogger('tornado.access')
+    access_log.setLevel(logging.INFO)
+    http_server = HTTPServer(WSGIContainer(APP), xheaders=True)
+    http_server.listen(5000, address='127.0.0.1')
     IOLoop.instance().start()
